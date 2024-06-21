99b2bb98d68c0154b2638d9eea6686f84c3fa432
AntoineP2 <antoine.vinsonneau34@gmail.com>
2024-06-21 16:48:10 +0200

    ShopSection

--- components/header/Header.component.tsx
+++ components/header/Header.component.tsx
@@ -17,8 +17,8 @@ const Header = () => {
                 <NavBarMobile />
             </div>
 
-            {showShop && <div className="fixed top-0 right-0 w-screen h-screen bg-black bg-opacity-70 flex justify-center items-center px-2 md:px-0">
-                <div className="relative bg-base-100 w-[70%] h-[80%] max-md:w-[90%]">
+            <div className={`fixed top-0 right-0 w-screen h-screen bg-black bg-opacity-80 flex justify-center items-center px-2 md:px-0 ${showShop ? "scale-100" : "scale-0"}`}>
+                <div className={`relative bg-base-100 w-[70%] h-[80%] max-md:w-[90%] transition duration-300 ${showShop ? "scale-100" : "scale-0"}`}>
                     <button className='absolute flex items-center justify-center top-5 right-5 max-md:active:scale-95 md:hover:scale-105 transition duration-150 ease-in-out'>
                         <IoMdCloseCircle size={40} className="text-error" onClick={closeShop} />
                     </button>
@@ -28,7 +28,6 @@ const Header = () => {
                     <div><StoreSection /></div>
                 </div>
             </div>
-            }
         </>
     )
 }
--- components/shop/StoreSection.component.tsx
+++ components/shop/StoreSection.component.tsx
@@ -8,7 +8,11 @@ const StoreSection = () => {
         <>
             <div className='max-h-[500px] overflow-y-auto py-10 md:py-16'>
                 <div className='flex flex-wrap justify-center gap-10  mx-2 md:mx-[100px]'>
-                    {StoreSectionIconList.map((icon) => <StoreSectionIcon key={icon.id} image={icon.image} title={icon.title} />)}
+                    {StoreSectionIconList.map((icon) =>
+                        <button key={icon.id} className="transition ease-in-out duration-150 md:hover:scale-105 max-md:active:scale-95">
+                            <StoreSectionIcon image={icon.image} title={icon.title} />
+                        </button>
+                    )}
                 </div>
             </div>
         </>
