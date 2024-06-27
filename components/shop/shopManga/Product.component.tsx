8cc6283dde61933c2b41f9c7a587c25c90ebd783
AntoineP2 <antoine.vinsonneau34@gmail.com>
2024-06-27 20:07:03 +0200

    add framer

--- components/shop/shopManga/Product.component.tsx
+++ components/shop/shopManga/Product.component.tsx
@@ -40,7 +40,7 @@ const Product: React.FC<ProductProps> = ({ product, colorBgHeader }) => {
     });
 
     const variants = {
-        hidden: { opacity: 0, y: 50 },
+        hidden: { opacity: 0, y: 35 },
         visible: { opacity: 1, y: 0 },
     };
 
