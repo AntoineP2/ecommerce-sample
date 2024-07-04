abbeba0d37d1e529fa6855482616b6ea2598f504
Antoine Vinsonneau <antoine.vinsonneau34@gmail.com>
2024-07-04 14:36:48 +0200

    correctif modal

--- components/header/Header.component.tsx
+++ components/header/Header.component.tsx
@@ -1,18 +1,21 @@
-"use client"
-import NavBarDesktop from './NavBarDesktop.component'
-import NavBarMobile from './NavBarMobile.component'
+"use client";
+import { useAppStore } from "@/lib/appStore";
+import NavBarDesktop from "./NavBarDesktop.component";
+import NavBarMobile from "./NavBarMobile.component";
 
 const Header = () => {
-    return (
-        <>
-            <div className="max-md:hidden h-full w-full">
-                <NavBarDesktop />
-            </div>
-            <div className="md:hidden h-full w-full">
-                <NavBarMobile />
-            </div>
-        </>
-    )
-}
+  const { showShop } = useAppStore();
+  return (
+    <>
+      
+        <div className="max-md:hidden h-full w-full">
+          <NavBarDesktop />
+        </div>
+        <div className="md:hidden h-full w-full">
+          <NavBarMobile />
+        </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
